fc180e02b98dbda028b823b6b884e7754b1c2c55
tomegoodson <158308016+tomegoodson@users.noreply.github.com>
2024-04-02 12:46:20 -0500

    handlinglab.tg

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,16 @@
-// Code Keypad Component Here
+import React from 'react';
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  const handleInputChange = () => {
+    console.log('Entering password...');
+  };
+
+  return (
+    <div>
+      <label htmlFor="password">Enter Password:</label>
+      <input type="password" id="password" onChange={handleInputChange} />
+    </div>
+  );
 }
 
 export default Keypad;
